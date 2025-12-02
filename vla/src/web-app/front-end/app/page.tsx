537be5dcb4d2c0c538a3013c624b70b1b0f19d2e
vishnunomadpersonal <vishnunomadpersonal@gmail.com>
2025-12-02 14:34:15 -0500

    Delete vla/src/web-app/front-end directory
    removing

--- vla/src/web-app/front-end/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { PromptControl } from '@/components/prompt-control'
-
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-background">
-      <PromptControl />
-    </div>
-  )
-}
